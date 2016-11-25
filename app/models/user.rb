0e0efb1fbd6ef5d2ae4d3c80f80f7d27ea13b0db
李衍禄 <L@liyanludeAir.lan>
2016-11-25 13:12:57 +0800

    login & logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { email.downcase! } #self.email = email.downcase 
   validates :name, presence: true, length: { maximum:50 },
                    uniqueness: { case_sensitive: false }
@@ -10,10 +11,33 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true,  length: { minimum: 6}
 
+  # 为了持久保存会话，在数据库中记住用户 
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token)) 
+  end
+
+  # 删除持久会话
+  def forget
+    update_attribute(:remember_token, nil)
+  end
+
+  # 如果指定的令牌和摘要匹配，返回 true 
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token) 
+  end
+
   # 返回指定字符串的哈希摘要 
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                       BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost) 
   end
+
+  # 返回一个随机令牌 
+  def self.new_token
+    SecureRandom.urlsafe_base64 
+  end
+
 end
