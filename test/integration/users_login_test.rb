49c2cf35a26a1e6c3cd9f74ad7c6dbdd01c52cb9
李衍禄 <L@liyanludeAir.lan>
2016-11-25 14:24:19 +0800

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -49,4 +49,14 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_template 'users/show'
     assert is_logged_in?
   end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+  end
+
+  test "login without remembering" do 
+    log_in_as(@user, remember_me: '0') 
+    assert_nil cookies['remember_token']
+  end
 end
